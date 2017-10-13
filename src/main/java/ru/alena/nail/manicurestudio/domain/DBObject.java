e7c0c5d9785f8aeda52148ddfe270daaf28feda0
agubanov <arteumg@mail.ru>
2017-10-13 18:21:13 +0300

    Добавлен базовый доменный объект для взаимодействия БД и сущность, описывающая услуги по маникюру

---
+++ src/main/java/ru/alena/nail/manicurestudio/domain/DBObject.java
@@ -0,0 +1,48 @@
+package ru.alena.nail.manicurestudio.domain;
+
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+/**
+ * Базовый класс для всех доменных объектов, используемых для взаимодействия с БД
+ *
+ * @author Gubanov Artyom.
+ */
+@MappedSuperclass
+public abstract class DBObject implements Serializable {
+
+    /**
+     * Получить уникальный идентификатор доменного объекта
+     *
+     * @return уникальный идентификатор доменного объекта
+     */
+    public abstract Long getId();
+
+
+    /**
+     * Проверить является доменный объект новым, не сохраненным в БД
+     *
+     * @return признак, того, что доменный объект является новым, т.е не сохранен в БД
+     */
+    public Boolean isNew() {
+        return getId() == null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DBObject dbObject = (DBObject) o;
+
+        return getId() != null ? getId().equals(dbObject.getId()) : dbObject.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
+
+}
+
+
---
+++ src/main/java/ru/alena/nail/manicurestudio/domain/Service.java
@@ -0,0 +1,75 @@
+package ru.alena.nail.manicurestudio.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+/**
+ * Услуга
+ *
+ * @author Gubanov Artyom.
+ */
+@Entity
+@Table(name = "service")
+public class Service extends DBObject {
+
+    /**
+     * Уникальный идентификатор услуги
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "service_id_seq")
+    @SequenceGenerator(name = "service_id_seq", sequenceName = "service_id_seq", allocationSize = 1)
+    private Long id;
+
+    /**
+     * Наименование услуги
+     */
+    @Column(name = "name")
+    private String name;
+
+
+    /**
+     * Описание услуги
+     */
+    @Column(name = "description")
+    private String description;
+
+    /**
+     * Цена на услугу
+     */
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+}
